daab6d69656afdf9e127b1ed6e142a7272e2a74f
Vlad Sheiko <sheiko.vlad@gmail.com>
2020-11-22 16:53:34 +0200

    Add project files.

---
+++ test_window1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace test_window1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Vvod_the_text = new System.Windows.Forms.Button();
+            this.TextForm = new System.Windows.Forms.Label();
+            this.Play_zanovo = new System.Windows.Forms.Button();
+            this.TextMin = new System.Windows.Forms.Label();
+            this.TextMax = new System.Windows.Forms.Label();
+            this.textBoxMax = new System.Windows.Forms.NumericUpDown();
+            this.textBoxMin = new System.Windows.Forms.NumericUpDown();
+            this.inputBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Vvod_the_text
+            // 
+            this.Vvod_the_text.Enabled = false;
+            this.Vvod_the_text.Location = new System.Drawing.Point(179, 250);
+            this.Vvod_the_text.Name = "Vvod_the_text";
+            this.Vvod_the_text.Size = new System.Drawing.Size(222, 23);
+            this.Vvod_the_text.TabIndex = 0;
+            this.Vvod_the_text.Text = "Ввод текста из поля";
+            this.Vvod_the_text.UseVisualStyleBackColor = true;
+            this.Vvod_the_text.Visible = false;
+            this.Vvod_the_text.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextForm
+            // 
+            this.TextForm.AutoSize = true;
+            this.TextForm.Enabled = false;
+            this.TextForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextForm.Location = new System.Drawing.Point(136, 123);
+            this.TextForm.Name = "TextForm";
+            this.TextForm.Size = new System.Drawing.Size(329, 48);
+            this.TextForm.TabIndex = 1;
+            this.TextForm.Text = "Введите число, которое по вашему\r\nмнению загадал бот, в поле ниже.";
+            this.TextForm.Visible = false;
+            this.TextForm.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Play_zanovo
+            // 
+            this.Play_zanovo.Enabled = false;
+            this.Play_zanovo.Location = new System.Drawing.Point(179, 288);
+            this.Play_zanovo.Name = "Play_zanovo";
+            this.Play_zanovo.Size = new System.Drawing.Size(222, 23);
+            this.Play_zanovo.TabIndex = 3;
+            this.Play_zanovo.Text = "Играть заново";
+            this.Play_zanovo.UseVisualStyleBackColor = true;
+            this.Play_zanovo.Visible = false;
+            this.Play_zanovo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TextMin
+            // 
+            this.TextMin.AutoSize = true;
+            this.TextMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextMin.Location = new System.Drawing.Point(210, 189);
+            this.TextMin.Name = "TextMin";
+            this.TextMin.Size = new System.Drawing.Size(31, 15);
+            this.TextMin.TabIndex = 5;
+            this.TextMin.Text = "Min";
+            this.TextMin.Visible = false;
+            // 
+            // TextMax
+            // 
+            this.TextMax.AutoSize = true;
+            this.TextMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextMax.Location = new System.Drawing.Point(340, 189);
+            this.TextMax.Name = "TextMax";
+            this.TextMax.Size = new System.Drawing.Size(34, 15);
+            this.TextMax.TabIndex = 6;
+            this.TextMax.Text = "Max";
+            this.TextMax.Visible = false;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(325, 215);
+            this.textBoxMax.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.textBoxMax.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(76, 20);
+            this.textBoxMax.TabIndex = 9;
+            this.textBoxMax.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.textBoxMax.Visible = false;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(179, 215);
+            this.textBoxMin.Maximum = new decimal(new int[] {
+            31999,
+            0,
+            0,
+            0});
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(76, 20);
+            this.textBoxMin.TabIndex = 10;
+            this.textBoxMin.Visible = false;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(179, 189);
+            this.inputBox.Maximum = new decimal(new int[] {
+            32000,
+            0,
+            0,
+            0});
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(222, 20);
+            this.inputBox.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.TextMax);
+            this.Controls.Add(this.TextMin);
+            this.Controls.Add(this.Play_zanovo);
+            this.Controls.Add(this.TextForm);
+            this.Controls.Add(this.Vvod_the_text);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "MainForm";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Vvod_the_text;
+        private System.Windows.Forms.Label TextForm;
+        private System.Windows.Forms.Button Play_zanovo;
+        private System.Windows.Forms.Label TextMin;
+        private System.Windows.Forms.Label TextMax;
+        private System.Windows.Forms.NumericUpDown textBoxMax;
+        private System.Windows.Forms.NumericUpDown textBoxMin;
+        private System.Windows.Forms.NumericUpDown inputBox;
+    }
+}
+
---
+++ test_window1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_window1
+{
+    public partial class MainForm : Form
+    {
+        Random R = new Random();
+        int chislo;
+        byte etap_pr ;
+        /*
+         * Console.Write("Введите до кокого числа бот будет придумивать число: ");
+            while (true)
+            {
+                try
+                {
+                    max_zagad_chislo = Convert.ToInt32(Console.ReadLine());
+                    zagad_chislo = R.Next(0, max_zagad_chislo);
+                    return;
+                }
+                catch
+                {
+                    WriteError();
+                }
+            }*/
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void etap()
+        {
+            switch (etap_pr)
+            {
+                case 0:
+                    TextMin.Visible = true;
+                    TextMax.Visible = true;
+                    textBoxMax.Visible = true;
+                    textBoxMin.Visible = true;
+                    Vvod_the_text.Visible = true;
+                    inputBox.Visible = false;
+                    TextForm.Visible = true;
+                    Play_zanovo.Enabled = true;
+                    Vvod_the_text.Enabled = true;
+                    inputBox.Enabled = true;
+                    TextForm.Enabled = true;
+                    Vvod_the_text.Text = "Ввести значения";
+                    break;
+            
+                case 1:
+                   
+                    inputBox.Visible = true;
+                    TextMin.Visible = false;
+                    TextMax.Visible = false;
+                    textBoxMax.Visible = false;
+                    textBoxMin.Visible = false;
+                    break;
+                case 2:
+                    inputBox.Visible = false;
+                    TextMin.Visible = false;
+                    TextMax.Visible = false;
+                    textBoxMax.Visible = false;
+                    textBoxMin.Visible = false;
+                    Vvod_the_text.Visible = false;
+                    Play_zanovo.Visible = true;
+                    Play_zanovo.Enabled = true;
+                    
+                    break;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*Play_zanovo.Enabled = true;
+            try
+            {
+                int res = int.Parse(inputBox.Text);
+                TextForm.Text = "" + (res + 1);
+                TextForm.ForeColor = Color.Green;
+            }
+            catch (FormatException x)
+            {
+                TextForm.Text = x.Message;
+                TextForm.ForeColor = Color.Red;
+            }*/
+            switch (etap_pr)
+            {
+                case 0:
+
+                    chislo = R.Next((int)textBoxMin.Value, (int)textBoxMax.Value);
+                    TextForm.Text = Convert.ToString(chislo);
+                    TextForm.Text = "Введите число, которое по вашему\nмнению загадал бот, в поле ниже.";
+                    etap_pr++;
+                break;
+
+                case 1:
+                    if (Convert.ToInt32(inputBox.Value) < chislo)
+                    {
+                        TextForm.Text = $"{inputBox} меньше загадонова числа,\nпопробуйте снова угадать число";
+                    }
+                    else if (Convert.ToInt32(inputBox.Value) > chislo)
+                    {
+                        TextForm.Text = $"{inputBox} больше загадонова числа,\nпопробуйте снова угадать число";
+                    }
+                    else if (Convert.ToInt32(inputBox.Value) == chislo)
+                    {
+                        TextForm.Text = "Молодци ви угадали число!!!\nЕсли хотите сиграть ещо раз нажмите на кнопку ниже";
+                        etap_pr=0;
+
+                    }
+                    break;
+            }
+            etap();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*int res;
+            if (int.TryParse(inputBox.Text, out res))
+            {
+                TextForm.Text = "" + (res + 1);
+                TextForm.ForeColor = Color.Green;
+            }
+            else
+            {
+                TextForm.Text = inputBox.Text;
+                TextForm.ForeColor = Color.Red;
+            }*/
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            TextMin.Visible = true;
+            TextMax.Visible = true;
+            textBoxMax.Visible = true;
+            textBoxMin.Visible = true;
+            Vvod_the_text.Visible = true;
+            inputBox.Visible = false;
+            TextForm.Visible = true;
+            Play_zanovo.Enabled = true;
+            Vvod_the_text.Enabled = true;
+            inputBox.Enabled = true;
+            TextForm.Enabled = true;
+            Vvod_the_text.Text = "Ввести значения";
+           // TextForm.Text = "Введите диапазон, в котором\nбот будет загадивать число.";
+            etap_pr = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void buttonWriteMinMax_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
